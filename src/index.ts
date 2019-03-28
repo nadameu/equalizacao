a20d6190d2bcecf91dbf00f08df5b5f9d86860a7
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-03-28 00:55:38 -0300

    Refatoração

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import './Object';
 import * as fs from 'fs';
 import dados from '../dados/distribuicaoreal1718';
 import { I as grupoI } from '../dados/grupos';
+import * as subsecoes from '../dados/subsecoes';
 import { fromDados } from './Ajuizamento';
 import { algoritmoV3 } from './algoritmoV3';
 import { show } from './Competencia';
 import { distribuicaoPorSorteio } from './distribuicaoPorSorteio';
+import './Object';
 
 function valueToString(x: string | number | { sigla: string }): string {
 	if (typeof x === 'string') return `"${x}"`;
@@ -13,30 +14,41 @@ function valueToString(x: string | number | { sigla: string }): string {
 	return `"${x.sigla}"`;
 }
 
-const ajuizamentos = fromDados(dados);
+const ajuizamentos = fromDados(dados, subsecoes);
 const distribuicoes = distribuicaoPorSorteio(ajuizamentos);
 const redistribuicoes = algoritmoV3(distribuicoes, grupoI);
 fs.writeFileSync(
 	'result.csv',
 	[
-		'"Processo";"Mês";"Competência";"Subseção de origem";"Vara de origem";"Juízo de origem";"Subseção de destino";"Vara de destino";"Juízo de destino"',
+		[
+			'Processo',
+			'Mês',
+			'Competência',
+			'Subseção de origem',
+			'Vara de origem',
+			'Juízo de origem',
+			'Subseção de destino',
+			'Vara de destino',
+			'Juízo de destino',
+		]
+			.map(x => `"${x}"`)
+			.join(';'),
 	]
 		.concat(
-			Array.from(redistribuicoes).map(
-				({ numeroProcesso, mes, subsecao, competencia, origem, destino }) =>
-					Object.values({
-						numeroProcesso,
-						mes,
-						competencia,
-						subsecao,
-						varaOrigem: origem.vara,
-						origem,
-						ssDestino: destino.vara.subsecao,
-						varaDestino: destino.vara,
-						destino,
-					})
-						.map(valueToString)
-						.join(';'),
+			Array.from(redistribuicoes).map(redist =>
+				[
+					redist.numeroProcesso,
+					redist.mes,
+					redist.competencia,
+					redist.subsecao,
+					redist.origem.vara,
+					redist.origem,
+					redist.destino.vara.subsecao,
+					redist.destino.vara,
+					redist.destino,
+				]
+					.map(valueToString)
+					.join(';'),
 			),
 		)
 		.join('\n'),
